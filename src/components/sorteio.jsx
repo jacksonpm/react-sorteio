6303b425c76459bc21343b857e7a51333bbb6efe
Jackson Moura <jacksonpmoura@hotmail.com>
2022-09-02 12:22:03 -0300

    Modificado sorteio

--- src/components/sorteio.jsx
+++ src/components/sorteio.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 import {Col, Row} from "react-bootstrap";
 import {randomInt} from "helpers/random";
 
@@ -8,22 +8,15 @@ export default function Sorteio() {
     const [sorteados, setSorteados] = useState([]);
     const [sorteado, setSorteado] = useState('');
 
-
-    useEffect(() => {
-        if (sorteado > 0) {
-            setSorteados([sorteado, ...sorteados])
-        }
-    }, [sorteado])
-
-
     return <div style={{height: "100vh"}}
-                className="d-flex justify-content-center align-items-center">
+                className="d-flex container justify-content-center align-items-center">
         <Row>
             <Col sm={12} md={12} lg={8}>
-                <div className="mb-2">
-                    Sorteio de Números
-                </div>
-                <form onSubmit={(e) => {
+                <div className="d-flex flex-column">
+                    <div className="mb-2">
+                        Sorteio de Números
+                    </div>
+                    <form onSubmit={(e) => {
                         e.preventDefault();
 
                         if (inicial > final) {
@@ -31,37 +24,41 @@ export default function Sorteio() {
                             return;
                         }
 
-                        setSorteado(randomInt(+inicial, +final))
+                        let numero = randomInt(+inicial, +final);
+                        setSorteado(numero)
+                        setSorteados([numero, ...sorteados])
                     }}>
-                    <div className="bg-white shadow-sm p-4 border rounded">
-                        <div className="form-group">
-                            <label
-                                className="form-label" htmlFor="inicio">Início</label>
-                            <input
-                                defaultValue=""
-                                type="number"
-                                onChange={(e) => setInicial(e.target.value)}
-                                required={true} name="inicio" className="form-control" value={inicial}/>
-                        </div>
-                        <div className="form-group">
-                            <label className="form-label" htmlFor="Fim">Fim</label>
-                            <input
-                                defaultValue=""
-                                onChange={(e) => setFinal(e.target.value)}
-                                type="number"
-                                required={true}
-                                name="fim" className="form-control" value={final}/>
-                        </div>
+                        <div
+                            className="bg-white shadow-sm p-4 border rounded">
+                            <div className="form-group">
+                                <label
+                                    className="form-label" htmlFor="inicio">Início</label>
+                                <input
+                                    defaultValue=""
+                                    type="number"
+                                    onChange={(e) => setInicial(e.target.value)}
+                                    required={true} name="inicio" className="form-control" value={inicial}/>
+                            </div>
+                            <div className="form-group">
+                                <label className="form-label" htmlFor="Fim">Fim</label>
+                                <input
+                                    defaultValue=""
+                                    onChange={(e) => setFinal(e.target.value)}
+                                    type="number"
+                                    required={true}
+                                    name="fim" className="form-control" value={final}/>
+                            </div>
 
-                        <button className="btn btn-lg btn-primary mt-3">
-                            Sortear
-                        </button>
-                    </div>
-                </form>
+                            <button className="btn btn-lg btn-primary mt-3">
+                                Sortear
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </Col>
             <Col
                 sm={12} md={12} lg={4}>
-                <div style={{minWidth: '14rem'}}>
+                <div className="d-flex flex-column">
                     <div className="mt-md-3 mt-lg-0">
                         Último Número Sorteado
                     </div>
@@ -76,9 +73,9 @@ export default function Sorteio() {
 
                     <div
                         style={{height: '30rem', overflowY: 'scroll'}}
-                        className="bg-white shadow-sm p-2 border rounded">
-                        {sorteados.map((item, i ) => {
-                            return <div className="border p-2 mt-2" key={i}>
+                        className="bg-white shadow-sm p-2 border rounded d-flex flex-column">
+                        {sorteados.map((item, i) => {
+                            return <div style={{flex: 1}} className="border p-2 m-1" key={i}>
                                 <span style={{fontSize: '2rem'}}>{item}</span>
                             </div>
                         })}
